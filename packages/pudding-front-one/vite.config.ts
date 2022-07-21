25922a3f714a0afc3e75880d08981e0e4f04145f
imber <a1157911285@163.com>
2022-07-21 17:30:12 +0800

    chore:引入CDN和包分析插件

--- packages/pudding-front-one/vite.config.ts
+++ packages/pudding-front-one/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { visualizer } from 'rollup-plugin-visualizer'
+import externalGlobals from 'rollup-plugin-external-globals'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  // publicDir: '.',
+  plugins: [
+    vue(),
+    visualizer({
+      // open: true// 打包完成后自动打开浏览器，显示产物体积报告
+    })
+  ],
+  server: {
+    open: true
+  },
+  build: {
+    rollupOptions: {
+      external: ['vue'],
+      plugins: [
+        externalGlobals({
+          vue: 'Vue'
+        })
+      ]
+    }
+  },
+  base: './' //打包路径变为相对路径，后面上线可能会修改
 })
